a8b2a815377e85e99899d5ae66fefb91037a7b64
alex <alexbnk@gmail.com>
2017-02-22 17:04:29 +0200

    reset cart total after [clear cart]

--- clearCart.js
+++ clearCart.js
@@ -2,9 +2,11 @@ var clearCart = function () {
 	// clears the selected items from cart list div
   $(".cart-list").empty();
 
+
   	// clears array in JS file
    	cart.splice(0, cart.length);
-
+    cartTotal= 0;
+    $(".total").empty()
+    $(".total").append(cartTotal);
 	//or cart = [];
 }
-
