e74fa9867d3b5b08881d453d4075219c7ea8146f
einarzb <EinarBarzilay@EinarBarzilays-MacBook.local>
2017-02-22 15:22:39 +0200

    created item

--- add-to-cart.js
+++ add-to-cart.js
@@ -1,5 +1,12 @@
-$('.add-to-cart').on('click', function () {
   // TODO: get the "item" object from the page
-  addItem(item);
-  updateCart();
+
+//this is .add-to-cart
+$('.add-to-cart').on('click', function () { 
+    var name = ($(this).parent().parent().data().name);
+    var price = ($(this).parent().parent().data().price);
+    var item = {name:name, price:price}
+//console.log(name + price); - test
+ addItem(item);
+ updateCart();
 });
+
