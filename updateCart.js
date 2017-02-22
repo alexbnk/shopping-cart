0157c31ec9ef2ecff277ea06979f58abbbd2cd0f
alex <alexbnk@gmail.com>
2017-02-22 16:37:28 +0200

    added total to cart , chaged the dolalr sign to the left

--- updateCart.js
+++ updateCart.js
@@ -1,10 +1,15 @@
+var cartTotal=0;
+
 var updateCart = function () {
   $(".cart-list").empty();
+  cartTotal = 0;
   for (var i=0 ; i< cart.length ; i++) {
-    $(".cart-list").append("<p>"+cart[i].name+"  "+cart[i].price+"$</p>");
-    viewCART(true);
+    $(".cart-list").append("<p>"+cart[i].name+" -  $"+cart[i].price+"</p>");
+   cartTotal = cartTotal+cart[i].price;
   }
-
+viewCART(true);
+  $(".total").empty()
+  $(".total").append(cartTotal);
   // TODO: Write this function. In this function we render the page.
   // Meaning we make sure that all our cart items are displayed in the browser.
   // Remember to empty the "cart div" before you re-add all the item elements.
