779c3bd38540fd16b949590790b4d5e7f173de51
Arik92 <arik92@gmail.com>
2017-02-22 15:38:23 +0200

    added an external function

--- view-cart.js
+++ view-cart.js
@@ -1,6 +1,13 @@
-var cartCount = 0;
+var cartCount=0;
 $('.view-cart').on('click', function () {
   // TODO: hide/show the shopping cart!
-  $('.shopping-cart').toggle(cartCount%2===0);
-  cartCount++;
+   if (cartCount%2===0){
+     viewCART(true);
+   }//if hidden
+   else viewCART(false);
+   cartCount++;
 });
+var viewCART = function(trigger){
+  // trigger is a boolean
+  $('.shopping-cart').toggle(trigger);
+}
