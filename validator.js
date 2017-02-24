479484f48456b6dc52c0258732ce725d1fb68434
Yishaik <yishai.k@gmail.com>
2017-02-24 05:03:53 +0200

    validation and alerts on success and failure of pname,pprice and ppic

--- validator.js
+++ validator.js
@@ -1,21 +1,87 @@
-var ppicVal=undefined;
-//var urlRegexp={"/(https?:\/\/.*\.(?:png|jpg))/i"
-var item = {};
-$("#preview").on("click", function(){
-item = {name:$("#pname").val(),price:$("#pprice").val(),image:$("#ppic").val()};
-console.log(item);
-display(item);
-});
-$("#ppic").keyup(function(){
-ppicVal = $("#ppic").val();
-if (RegExp(/(https?:\/\/.*\.(?:png|jpg))/i).test(ppicVal)){//testing RegExp
-$('.alert-success').toggle(true);
-}
-});
-// on click submit add to products array
-$("#submit").on("click", function(){
-  item = {name:$("#pname").val(),price:$("#pprice").val(),image:$("#ppic").val()};
-products.push(item);
-$(this).closest('form').find("input[type=text], textarea").val("");
-});
-//display(item);
+      //validator function gets boolean
+      // var validate = true;
+      // var validator = function(validate){
+      //   if (validate){
+      //     alert("Validator running")
+      //      $('.alert-success').toggle(true);//open success alert
+      // // setTimeout($(".alert-success").toggle(false),[2000]);
+      //      //remove success alert - should run after 3 sec timeout
+      //   } else{
+      //
+      //   }
+      // }
+      // success function opens the success alert
+      var openSuccessAlert = function(Boolean) {
+          //alert("successful validation will open success alert")
+          $('.alert-success').toggle(true);
+      }
+      // close toggle after 2000ms
+      var closeSuccessAlert = function() {
+          $(".alert-success").toggle(false)
+      }
+      var openFailAlert = function() {
+        $(".alert-danger").toggle(true);
+      }
+      var closeFailAlert = function() {
+        $(".alert-danger").toggle(false);
+      }
+
+      $("#pname").keyup(function(){
+        pnameVal = $("#pname").val();
+        if (RegExp(/^[a-z0-9]+$/i).test(pnameVal)){
+        closeFailAlert();
+        openSuccessAlert();
+    } else {
+      closeSuccessAlert()
+      openFailAlert();
+    }})
+      $("#pprice").keyup(function() {
+          ppriceVal = $("#pprice").val();
+          if (RegExp(/^\d+$/).test(ppriceVal)) {
+              //alert("pprice validation")
+              closeFailAlert();
+              openSuccessAlert();
+          } else {
+            closeSuccessAlert()
+            openFailAlert();
+          };
+      });
+      $("#ppic").keyup(function() {
+          ppicVal = $("#ppic").val();
+          if (RegExp(/(https?:\/\/.*\.(?:png|jpg))/i).test(ppicVal)) { //testing RegExp
+            openSuccessAlert();
+            closeFailAlert()
+          } else {
+            closeSuccessAlert()
+            openFailAlert();
+          } ;
+      });
+      var item = {};
+      //runs when clicking the preview
+      //creates an item object and display preview
+      //closes open alert
+      $("#preview").on("click", function() {
+          item = {
+              name: $("#pname").val(),
+              price: $("#pprice").val(),
+              image: $("#ppic").val()
+          };
+          console.log(item);
+          display(item);
+          closeSuccessAlert(false);
+          closeFailAlert(false);
+      });
+      // on click submit add current product details to products array
+      $("#submit").on("click", function() {
+          item = {
+              name: $("#pname").val(),
+              price: $("#pprice").val(),
+              image: $("#ppic").val()
+          };
+          products.push(item);
+          console.log(products.length+" objects in products array "+products);
+          $(this).closest('form').find("input[type=text], textarea").val("");
+          closeSuccessAlert();
+          closeFailAlert();
+      });
+      //display(item);
