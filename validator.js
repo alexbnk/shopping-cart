6a1be9a02b10f4dc8ae5be00878f731936d91292
Yishaik <yishai.k@gmail.com>
2017-02-24 03:04:02 +0200

    preview run display function, submit add object to products, correct URL shows success message

--- validator.js
+++ validator.js
@@ -1,11 +1,21 @@
-$('#admin-form').parsley();
+var ppicVal=undefined;
+//var urlRegexp={"/(https?:\/\/.*\.(?:png|jpg))/i"
 var item = {};
 $("#preview").on("click", function(){
 item = {name:$("#pname").val(),price:$("#pprice").val(),image:$("#ppic").val()};
 console.log(item);
-products.push(item);
+display(item);
 });
 $("#ppic").keyup(function(){
-//console.log(item);
-//display(item);
+ppicVal = $("#ppic").val();
+if (RegExp(/(https?:\/\/.*\.(?:png|jpg))/i).test(ppicVal)){//testing RegExp
+$('.alert-success').toggle(true);
+}
 });
+// on click submit add to products array
+$("#submit").on("click", function(){
+  item = {name:$("#pname").val(),price:$("#pprice").val(),image:$("#ppic").val()};
+products.push(item);
+$(this).closest('form').find("input[type=text], textarea").val("");
+});
+//display(item);
